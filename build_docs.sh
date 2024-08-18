4d6da061388fde57784f98cd37b6bdd98328b26a
Jenly <jenly1314@gmail.com>
2024-08-18 16:37:50 +0800

    docs

--- build_docs.sh
+++ build_docs.sh
@@ -11,7 +11,7 @@ mv camera-scan/build/dokka/html/* docs/api
 # Copy in special files that GitHub wants in the project root.
 GITHUB_URL=https://github.com/jenly1314/CameraScan/
 sed '/<!-- end -->/q' README.md > docs/index.md
-sed -i "s|(app|(${GITHUB_URL}blob/master/app|g" docs/index.md
+sed -i "s|](app|](${GITHUB_URL}blob/master/app|g" docs/index.md
 cat CHANGELOG.md | grep -v '## 版本日志' > docs/changelog.md
 
 # Build the site locally
